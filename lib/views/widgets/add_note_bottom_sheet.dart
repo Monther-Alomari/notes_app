03688783b519699288ac9643f7851fafe61f7c74
Monther Alomari <montheralomari896@gmail.com>
2024-10-11 16:26:04 +0300

    Your commit message

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notesapp/views/widgets/custom_bottom.dart';
-import 'package:notesapp/views/widgets/custom_text_field.dart';
+import 'package:notesapp/views/widgets/add_note_form.dart';
 
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,22 +7,10 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16,),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16,),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(hint: "title",),
-            SizedBox(height: 16,),
-            CustomTextField(hint: "Content",maxLines: 5,),
-            SizedBox(height: 35,),
-            CustomBottom(),
-            SizedBox(height: 16,),
-          ],
-        ),
+        child: AddNoteForm(),
       ),
     );
   }
--- lib/views/widgets/custom_bottom.dart
+++ lib/views/widgets/custom_bottom.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/constants.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key});
-
+  const CustomBottom({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      // better than duoble.infenity,
-      height: 55,
-      decoration: BoxDecoration(
-          color: kprimaryColor,
-          borderRadius: BorderRadius.circular(8)
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        // better than duoble.infenity,
+        height: 55,
+        decoration: BoxDecoration(
+            color: kprimaryColor, borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+            child: Text(
+          "Add",
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        )),
       ),
-      child: const Center(child: Text("Add",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),)),
-
     );
   }
 }
